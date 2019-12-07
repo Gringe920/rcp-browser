e142495d231c7b5b308312fd37d8a67764647938
xmm <xiemei1996@163.com>
2019-12-07 21:20:34 +0800

    save

---
+++ plugins/components.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+const header = () => import(/* webpackChunkName: "components" */ '../components/Header');
+const footer = () => import(/* webpackChunkName: "components" */ '../components/Footer');
+Vue.component('Header', header);
+Vue.component('Footer', footer);
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-  
+  <div class="headerall">
+    <div class="header_l">
+      <img src="" alt="" srcset="">
+      <div>RCP</div>
+    </div>
+    <div class="header_r">
+      <div>English</div>
+      <img src="" alt="" srcset="">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +17,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.headerall{
+  background: pink;
+  width: 100%;
+  .header_l{
+    img{
+
+    }
+    div{
+
+    }
+  }
+}
 
 </style>
